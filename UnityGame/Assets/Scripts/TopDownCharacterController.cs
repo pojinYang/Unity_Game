9da98604fa71e9b1aff5f0a032cfc5a90af58881
pojinYang <117494636+pojinYang@users.noreply.github.com>
2024-02-08 10:45:03 +0800

    add collision
    add collision

--- UnityGame/Assets/Scripts/TopDownCharacterController.cs
+++ UnityGame/Assets/Scripts/TopDownCharacterController.cs
@@ -22,37 +22,36 @@ namespace Cainos.PixelArtTopDown_Basic
         private void Update()
         {
             Vector2 dir = Vector2.zero;
-            if(canPlayerMove){
-            
-               
-            
-                if (Input.GetKey(KeyCode.A))
-                {
-                    dir.x = -1;
-                    animator.SetInteger("Direction", 3);
-                }
-                else if (Input.GetKey(KeyCode.D))
-                {
-                    dir.x = 1;
-                    animator.SetInteger("Direction", 2);
-                }
-
-                if (Input.GetKey(KeyCode.W))
-                {
-                    dir.y = 1;
-                    animator.SetInteger("Direction", 1);
-                }
-                else if (Input.GetKey(KeyCode.S))
-                {
-                    dir.y = -1;
-                    animator.SetInteger("Direction", 0);
-                }
-
-                dir.Normalize();
-                animator.SetBool("IsMoving", dir.magnitude > 0);
-
-                GetComponent<Rigidbody2D>().velocity = speed * dir;
+            if(!canPlayerMove)
+            {
+                dir.x = 0;
             }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                dir.x = -1;
+                animator.SetInteger("Direction", 3);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                dir.x = 1;
+                animator.SetInteger("Direction", 2);
+            }
+
+            else if (Input.GetKey(KeyCode.W))
+            {
+                dir.y = 1;
+                animator.SetInteger("Direction", 1);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                dir.y = -1;
+                animator.SetInteger("Direction", 0);
+            }
+
+            dir.Normalize();
+            animator.SetBool("IsMoving", dir.magnitude > 0);
+
+            GetComponent<Rigidbody2D>().velocity = speed * dir;
         }
     }
 }
