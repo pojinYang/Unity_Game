730fccd7947fadbebd6226daf531c69f5983889c
WgLy <102111george@gmail.com>
2024-02-09 09:50:39 +0800

    backpack

--- UnityGame/Assets/Scripts/BackPackItem.cs
+++ UnityGame/Assets/Scripts/BackPackItem.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class BackPackItem : MonoBehaviour
 {
     // Start is called before the first frame update
-    public bool slate = false,fire=false,flower=false,ash=false;
-    
+    public bool invisible_lens = false; //隱形眼鏡  //燒不了
+    public bool herb = false;  //藥材  
+    public bool stone_plate = false;  //石板
+    public bool fire_flower = false;  //火焰花
+    public bool plant_ginhua = false;  //植物精華
+    public bool fire = false;  //祭祀之火
+    public bool treasure = false;  //祕寶
+    public bool sword = false; //聖劍 
+    public bool sword_key = false;   //燒不了
+    public bool treasure_key = false;    //燒不了
+    public bool ash = false;
+    public bool fire_ginhua = false;  //火焰精華
+    public bool rocket = false; //火箭
+
+
     void Start()
     {
         
