956c64b4453b0d75b706b62cce3386b2d9b43bae
WgLy <102111george@gmail.com>
2024-02-09 20:35:43 +0800

    box talk

--- UnityGame/Assets/Scripts/ForItem/wood_box_controller.cs
+++ UnityGame/Assets/Scripts/ForItem/wood_box_controller.cs
@@ -3,62 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using Flower;
 
-public class next2stairController : MonoBehaviour
+public class wood_box_controller : MonoBehaviour
 {
     public bool isNear = false;
     FlowerSystem fs;
-    // Start is called before the first frame update
-    bool setup = false;
     GameObject hint;
     GameObject backpack;
     Vector3 Pos = new Vector3(0, 0, 0);
+    bool setup = false;
+    // Start is called before the first frame update
     void Start()
-    {   
+    {
         backpack = GameObject.Find("Backpack");
         Pos = transform.localPosition;
-        Pos.x =1.76f;
-        Pos.y =-9.28f;
         hint = GameObject.Find("hint");
         fs= FlowerManager.Instance.GetFlowerSystem("default");
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if(isNear){
-            
-
-            
-
-            
-            
-            
             if(Input.GetKeyDown(KeyCode.Space)){
                 if(!setup){
-                    if(backpack.GetComponent<BackPackItem>().stage==1)
-                        fs.ReadTextFromResource("stage 1");
-                    else{
-                        fs.ReadTextFromResource("other");
-                        
-                    }
                     setup = true;
-                }else{
-
-                    fs.Next();
-
+                    fs.ReadTextFromResource("wrongbox");
                 }
-                                    
-                                
-
             }
-                
         }
-            
-
-            
     }
+
     void OnTriggerEnter2D(Collider2D other) {
         //Debug.Log("next2stair物體進入觸發器");
         Pos.y +=0.9f;
@@ -66,15 +40,13 @@ public class next2stairController : MonoBehaviour
         Pos.y -=0.9f;
         hint.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         isNear = true;
+
     }
 
     void OnTriggerExit2D(Collider2D other) {
         //Debug.Log("next2stair物體離開觸發器");
-        
-
-        isNear = false;
         setup = false;
-        
+        isNear = false;
         hint.transform.localScale = new Vector3(0, 0, 0);
     }
 }
