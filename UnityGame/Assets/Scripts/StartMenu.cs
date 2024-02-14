6e54874816cca61a6dd4bfa747b61b6705c06557
WgLy <102111george@gmail.com>
2024-02-14 21:26:43 +0800

    StartMenu & ReadMe

--- UnityGame/Assets/Scripts/StartMenu.cs
+++ UnityGame/Assets/Scripts/StartMenu.cs
@@ -7,6 +7,16 @@ public class StartMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     FlowerSystem fs;
+    public bool isReadMe = false;
+    public GameObject readme0;
+    public GameObject text0;
+    public void Start()
+    {
+        readme0 = GameObject.Find("readme_square");
+        text0 = GameObject.Find("Text");
+    }
+
+
     public void playgame()
     {   
         fs = FlowerManager.Instance.CreateFlowerSystem("default", false);
@@ -15,6 +25,21 @@ public class StartMenu : MonoBehaviour
         
 
     }
+
+    public void readme()
+    {   
+        if(isReadMe) isReadMe = false;
+        else isReadMe = true;
+        if(isReadMe){
+            readme0.transform.localScale = new Vector3(11,9,0);
+            text0.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+        }
+        else{
+            readme0.transform.localScale = new Vector3(0,0,0);
+            text0.transform.localScale = new Vector3(0,0,0);
+        } 
+    }
+
 
     // Update is called once per frame
     public void Quitgame()
